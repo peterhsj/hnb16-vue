541642f32ff409a5dcb83407142e2c209a5efd55
Peter <peterhsj@gmail.com>
2026-06-09 09:40:11 +0800

    feat: CDS 過期案件主動註銷

--- src/pages/draftAdjustment.vue
+++ src/pages/draftAdjustment.vue
@@ -97,6 +97,32 @@
         @on-close="messageClose"
         @prompt-confirm="messageConfirm"
       />
+      <!-- Lc Dialog -->
+      <LcDialog
+        v-model:lc-dialog="lcDialog"
+        :bene-type="''"
+        :is-show-notice="true"
+        :is-show-version="true"
+        :lc-no="lcNo"
+        @on-close="lcDialogClose"
+        @open-lc-detail="handleOpenLcDetail"
+        @open-notice-detail="handleOpenNoticeDetail"
+      />
+      <!-- Lc Detail Dialog (版本詳細) -->
+      <LcDialog
+        v-model:lc-dialog="lcDetailDialog"
+        :bene-type="''"
+        :lc-no="lcDetailNo"
+        @on-close="lcDetailDialogClose"
+      />
+      <!-- 信用狀修改通知書 Notice Dialog -->
+      <NoticeDialog
+        v-model:notice-dialog="noticeDialog"
+        :is-show-lc="true"
+        :notice-no="noticeNo"
+        @on-close="noticeDialogClose"
+        @open-lc-detail="handleOpenLcDetail"
+      />
     </v-container>
   </div>
 </template>
@@ -113,6 +139,15 @@
   const { handleApiError } = useApiErrorHandler()
   const isLoading = ref(false)
   const isShowList = ref(true)
+  // Lc Dialog
+  const lcDialog = ref(false)
+  const lcNo = ref<string>('')
+  // Lc Detail Dialog (版本詳細)
+  const lcDetailDialog = ref(false)
+  const lcDetailNo = ref<string>('')
+  // Notice Dialog
+  const noticeDialog = ref(false)
+  const noticeNo = ref<string>('')
 
   const breadcrumbs = [
     { title: '首頁', to: '/' },
@@ -216,12 +251,44 @@
     console.log('View Draft:', draftNo)
   }
 
-  function handleLcView (lcNo: string): void {
-    console.log('View LC:', lcNo)
-  }
-
   function handlerCredit (lcNo: string): void {
     console.log('Edit item:', lcNo)
+  }
+
+  // 查看信用狀 Lc Dialog
+  function handleLcView (value: string): void {
+    lcNo.value = value
+    lcDialog.value = true
+  }
+
+  // 離開 Lc Dialog
+  function lcDialogClose (): void {
+    lcDialog.value = false
+    lcNo.value = ''
+  }
+
+  // 開啟版本詳細 Dialog
+  function handleOpenLcDetail (value: string): void {
+    lcDetailNo.value = value
+    lcDetailDialog.value = true
+  }
+
+  // 離開 Lc Detail Dialog
+  function lcDetailDialogClose (): void {
+    lcDetailDialog.value = false
+    lcDetailNo.value = ''
+  }
+
+  // 開啟修改通知書 Detail Dialog
+  function handleOpenNoticeDetail (value: string): void {
+    noticeNo.value = value
+    noticeDialog.value = true
+  }
+
+  // 離開修改通知書 Detail Dialog
+  function noticeDialogClose (): void {
+    noticeDialog.value = false
+    noticeNo.value = ''
   }
 
   onMounted(fetchTableList)
